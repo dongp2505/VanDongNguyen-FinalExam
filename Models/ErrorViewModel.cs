0a36725878e1b7212d88ef0bd32d3fa22b9c916a
dongp2505 <dongp2505@gmail.com>
2025-12-10 19:17:02 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VanDongNguyen_FinalExam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
